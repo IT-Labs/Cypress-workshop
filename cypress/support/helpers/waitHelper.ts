7d6a92ded787dab92865e53f3f242b6973441664
Sanja Paskova <sanja.paskova@it-labs.com>
2021-03-25 11:27:26 +0100

    fixed wait helper

--- cypress/support/helpers/waitHelper.ts
+++ cypress/support/helpers/waitHelper.ts
@@ -1,52 +1,12 @@
 /// <reference types = "cypress"/>
-export class ValidationHelper {
+export class WaitHelper {
 
-    static verifyElementHaveCss(
+    static waitForElementVisibility(
       targetElement,
-      css) {
-      return targetElement.should('have.css', css, targetElement);
-    }
-  
-    static verifyElementHaveClass(
-      targetElement,
-      css) {
-      return targetElement.should('have.class', css, targetElement);
-    }
-  
-    static verifyElementContainText(
-      targetElement,
-      text) {
-      return targetElement.should('contain', text, targetElement);
-    }
-  
-    static verifyElementHaveValue(
-      targetElement,
-      text) {
-      return targetElement.should('have.value', text, targetElement);
-    }
-  
-    static verifyElementEnabled(
-      targetElement) {
-      return targetElement.should('not.be.disabled', targetElement);
-    }
-  
-    static verifyElementDisabled(
-      targetElement) {
-      return targetElement.should('be.disabled', targetElement);
-    }
-  
-    static verifyTitle(titleObject, titleText) {
-      return titleObject.should('eq', titleText);
-    }
-  
-    static verifyElementHidden(
-      targetElement) {
-      return targetElement.should('be.hidden', targetElement);
-    }
-    
-    static verifyElementVisible(
-      targetElement) {
-      return targetElement.should('be.visible', targetElement);
+      message = 'Element should be visible'
+    ) {
+      return targetElement.should('be.visible', targetElement + message);
     }
   
   }
+  
